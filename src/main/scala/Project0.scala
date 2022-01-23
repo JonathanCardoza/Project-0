fee0dcde1b355af896beaa6648ef47fc2fddaf44
RevJonathan <97978787+RevJonathan@users.noreply.github.com>
2022-01-23 14:10:40 -0800

    Added functionality to user

--- src/main/scala/Project0.scala
+++ src/main/scala/Project0.scala
@@ -17,7 +17,7 @@ object Project0 {
     }
     if(accessLevel.equals("Admin")){
       println("Welcome " + username + ", what would you like to do? ")
-      println("Press:\n 1. View list of Clients' ID\n 2. View list of Clients' name\n 3. View list of clients' account type\n 4. View when last paid.")
+      println("Press:\n 1. View list of Clients' ID\n 2. View list of Clients' name\n 3. View list of clients' account type\n 4. Add a new Client.")
       val adminRes = readLine()
       if(adminRes.equals("1")) {
         println("Here is the list of Clients' ID:")
@@ -28,9 +28,20 @@ object Project0 {
       } else if(adminRes.equals("3")){
         println("Here is the list of Clients' account type:")
         getClientsAcctType()
+      }else if(adminRes.equals("4")){
+        addAClient()
       }
     } else if(accessLevel.equals("user")){
       println("Welcome " + username + ", what would you like to do?" )
+      println("Press:\n 1. View Balance\n 2. View last paid\n 3. Make a deposit")
+      val userRes = readLine()
+      if(userRes.equals("1")){
+        viewBalance()
+      }else if(userRes.equals("2")){
+        viewLastPaid()
+      }else if(userRes.equals("3")){
+        makeDeposit()
+      }
 
     }
 
@@ -127,6 +138,108 @@ object Project0 {
       }
       if(con != null)
         System.out.println("Database connection is successful!")
+    } catch {
+      case e: Exception =>
+        e.printStackTrace()
+    }
+    return con
+  }
+  def viewBalance() : Connection = {
+    val connection ="jdbc:mysql://127.0.0.1:3306/UserInfo"
+    val userName = "root"
+    val password ="Special7791@"
+    val driver = "com.mysql.cj.jdbc.Driver"
+    val con = DriverManager.getConnection(connection, userName, password)
+
+    try {
+      Class.forName(driver)
+      val con = DriverManager.getConnection(connection, userName, password)
+      val acctID = readLine("Please enter your account ID:")
+      val query = "SELECT Balance FROM BankInfo WHERE AcctID = '"+acctID+"'"
+      val statement = con.createStatement()
+      val resultSet = statement.executeQuery(query)
+      while(resultSet.next()) {
+        println("Your Balance is:")
+        val ID = resultSet.getInt("Balance" )
+        println("$" + ID)
+      }
+      if(con != null)
+        System.out.println("Database connection is successful!")
+    } catch {
+      case e: Exception =>
+        e.printStackTrace()
+    }
+    return con
+  }
+  def viewLastPaid() : Connection = {
+    val connection ="jdbc:mysql://127.0.0.1:3306/UserInfo"
+    val userName = "root"
+    val password ="Special7791@"
+    val driver = "com.mysql.cj.jdbc.Driver"
+    val con = DriverManager.getConnection(connection, userName, password)
+
+    try {
+      Class.forName(driver)
+      val con = DriverManager.getConnection(connection, userName, password)
+      val acctNum = readLine("Please enter your account number:")
+      val query = "SELECT LastPaid FROM BankInfo WHERE acctNum = '"+acctNum+"'"
+      val statement = con.createStatement()
+      val resultSet = statement.executeQuery(query)
+      while(resultSet.next()) {
+        val lastPaid = resultSet.getDate("LastPaid")
+        println("The last time you were paid was on:\n" +lastPaid)
+      }
+      if(con != null)
+        System.out.println("Database connection is successful!")
+    } catch {
+      case e: Exception =>
+        e.printStackTrace()
+    }
+    return con
+  }
+
+  def makeDeposit() : Connection = {
+    val connection ="jdbc:mysql://127.0.0.1:3306/UserInfo"
+    val userName = "root"
+    val password ="Special7791@"
+    val driver = "com.mysql.cj.jdbc.Driver"
+    val con = DriverManager.getConnection(connection, userName, password)
+
+    try {
+      Class.forName(driver)
+      val con = DriverManager.getConnection(connection, userName, password)
+      val acctID= readLine("Please enter AcctID: ")
+      val makeDeposit = readLine("Please enter amount you would like to deposit in USD: ")
+      val query = "UPDATE BankInfo SET Balance = '"+makeDeposit+"' WHERE AcctID = '"+acctID+"'"
+      val statement = con.createStatement()
+      val resultSet = statement.executeUpdate(query)
+      if(con != null)
+        System.out.println("Deposit successfully completed!")
+    } catch {
+      case e: Exception =>
+        e.printStackTrace()
+    }
+    return con
+  }
+
+  def addAClient() : Connection = {
+    val connection ="jdbc:mysql://127.0.0.1:3306/UserInfo"
+    val userName = "root"
+    val password ="Special7791@"
+    val driver = "com.mysql.cj.jdbc.Driver"
+    val con = DriverManager.getConnection(connection, userName, password)
+
+    try {
+      Class.forName(driver)
+      val con = DriverManager.getConnection(connection, userName, password)
+      val id = readLine("Please assign an ID number for the client: ")
+      val name = readLine("Please enter a name for the client: ")
+      val acctType = readLine("Please enter account type for client: ")
+      val query = "INSERT INTO ClientInfo VALUES('"+id+"','"+name+"','"+acctType+"')"
+      val statement = con.createStatement()
+      val resultSet = statement.executeUpdate(query)
+      if(con != null)
+        System.out.println("Successfully added client's information!")
     } catch {
       case e: Exception =>
         e.printStackTrace()
