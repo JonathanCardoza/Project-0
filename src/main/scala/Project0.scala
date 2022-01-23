52b27ad353d562c6e6e37a1f14dd48db27d9b831
RevJonathan <97978787+RevJonathan@users.noreply.github.com>
2022-01-22 20:56:35 -0800

    Added the user acct.

--- src/main/scala/Project0.scala
+++ src/main/scala/Project0.scala
@@ -10,26 +10,31 @@ object Project0 {
     val username = readLine("Please enter your username: ")
     val password = readLine("Please enter your password ")
     val accessLevel = username
-    if(!username.equals("Admin") || !password.equals("root")){
+    if((!username.equals("Admin") || !password.equals("root")) &&
+      (!username.equals("user") || !password.equals("user"))){
       println("Incorrect login credentials!")
       return
     }
+    if(accessLevel.equals("Admin")){
+      println("Welcome " + username + ", what would you like to do? ")
+      println("Press:\n 1. View list of Clients' ID\n 2. View list of Clients' name\n 3. View list of clients' account type\n 4. View when last paid.")
+      val adminRes = readLine()
+      if(adminRes.equals("1")) {
+        println("Here is the list of Clients' ID:")
+        getClientsID()
+      } else if(adminRes.equals("2")){
+        println("Here is the list of clients:")
+        getClientsName()
+      } else if(adminRes.equals("3")){
+        println("Here is the list of Clients' account type:")
+        getClientsAcctType()
+      }
+    } else if(accessLevel.equals("user")){
+      println("Welcome " + username + ", what would you like to do?" )
 
-    println("Welcome " + username + " what would you like to do? ")
-    println("Press:\n 1. To view balance\n 2. Get account number or\n 3. View account type\n 4. View when last paid.")
-    val adminRes = readLine()
-
-    if(adminRes.equals("1")) {
-      println("Your balance is:")
-      getAcctBalance()
-    } else if(adminRes.equals("2")){
-      println("Your account number is:")
-      getAcctNum()
     }
 
   }
-
-
 
   def dbConnection (): Connection = {
     val connection ="jdbc:mysql://127.0.0.1:3306/UserInfo"
@@ -41,15 +46,6 @@ object Project0 {
     try {
       Class.forName(driver)
       val con = DriverManager.getConnection(connection, userName, password)
-      val query = "SELECT * FROM ClientInfo"
-      val statement = con.createStatement()
-      val resultSet = statement.executeQuery(query)
-
-      while(resultSet.next()) {
-        val id = resultSet.getInt("ID")
-        println(id)
-      }
-      statement.close()
       if(con != null)
         System.out.println("Database connection is successful!")
     } catch {
@@ -60,7 +56,7 @@ object Project0 {
 
   }
 
-  def getAcctBalance() : Connection = {
+  def getClientsID() : Connection = {
     val connection ="jdbc:mysql://127.0.0.1:3306/UserInfo"
     val userName = "root"
     val password ="Special7791@"
@@ -70,12 +66,12 @@ object Project0 {
     try {
       Class.forName(driver)
       val con = DriverManager.getConnection(connection, userName, password)
-      val query = "SELECT Balance FROM BankInfo"
+      val query = "SELECT ID FROM ClientInfo"
       val statement = con.createStatement()
       val resultSet = statement.executeQuery(query)
       while(resultSet.next()) {
-        val balance = resultSet.getInt("Balance")
-        println(balance)
+        val ID = resultSet.getInt("ID")
+        println(ID)
       }
       if(con != null)
         System.out.println("Database connection is successful!")
@@ -86,7 +82,7 @@ object Project0 {
     return con
   }
 
-  def getAcctNum() : Connection = {
+  def getClientsName() : Connection = {
     val connection ="jdbc:mysql://127.0.0.1:3306/UserInfo"
     val userName = "root"
     val password ="Special7791@"
@@ -96,12 +92,12 @@ object Project0 {
     try {
       Class.forName(driver)
       val con = DriverManager.getConnection(connection, userName, password)
-      val query = "SELECT AcctNum FROM BankInfo"
+      val query = "SELECT Name FROM ClientInfo"
       val statement = con.createStatement()
       val resultSet = statement.executeQuery(query)
       while(resultSet.next()) {
-        val acctNum = resultSet.getInt("AcctNum")
-        println(acctNum)
+        val clientsName = resultSet.getString("Name")
+        println(clientsName)
       }
       if(con != null)
         System.out.println("Database connection is successful!")
@@ -112,6 +108,30 @@ object Project0 {
     return con
   }
 
+  def getClientsAcctType() : Connection = {
+    val connection ="jdbc:mysql://127.0.0.1:3306/UserInfo"
+    val userName = "root"
+    val password ="Special7791@"
+    val driver = "com.mysql.cj.jdbc.Driver"
+    val con = DriverManager.getConnection(connection, userName, password)
 
+    try {
+      Class.forName(driver)
+      val con = DriverManager.getConnection(connection, userName, password)
+      val query = "SELECT AcctType FROM ClientInfo"
+      val statement = con.createStatement()
+      val resultSet = statement.executeQuery(query)
+      while(resultSet.next()) {
+        val acctType = resultSet.getString("AcctType")
+        println(acctType)
+      }
+      if(con != null)
+        System.out.println("Database connection is successful!")
+    } catch {
+      case e: Exception =>
+        e.printStackTrace()
+    }
+    return con
+  }
 
 }
